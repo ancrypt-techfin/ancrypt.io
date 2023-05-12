49d60f0f6fe335a83123a40044f4538bfc5a0b66
danishberg <70341272+danishberg@users.noreply.github.com>
2023-05-12 16:50:14 +0800

    update

--- .next/static/HgGmGdhm3Ezp3LJgt4vid/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/94726e6d-625ea5860223ed40.js","static/chunks/91-3e1657e76ca5124e.js","static/chunks/pages/index-0a4cdc13e025f2ad.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
